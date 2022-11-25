7af2e21c00f0be596c275da979af73aedbba2ab9
Tim Owens <58872684+towens4@users.noreply.github.com>
2022-11-25 16:29:49 +1100

    Changed

--- WellsBookstore/Program.cs
+++ WellsBookstore/Program.cs
@@ -9,6 +9,7 @@ builder.Services.AddDbContext<WellsBookstoreDBContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("DESKTOP-V1MK9K0\\SQLEXPRESS"));
 });
+
 builder.Services.AddMvc();
 
 var app = builder.Build();
